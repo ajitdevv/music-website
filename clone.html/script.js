1d780f9b5df03849814130113a011c6e050bc515
ajitdevv <ajitdev014@gmail.com>
2025-09-16 22:42:00 +0530

    commit

--- clone.html/script.js
+++ clone.html/script.js
@@ -1,17 +1,13 @@
 //! add extra ====> add your card using only name and pitcher
 let btn = document.querySelector(".btnn");
-
 btn.addEventListener("click", function () {
     if (document.body.classList.contains('dark')) {
-        // Agar body mein 'dark' class hai, toh use hata ke 'light' add karo
         document.body.classList.remove('dark');
         document.body.classList.add('light');
-    } else if (document.body.classList.contains('light')) {
-        // Agar 'light' class hai, toh 'dark' add karo
+    } else if (document.body.classList.contains('light')) {     
         document.body.classList.remove('light');
         document.body.classList.add('dark');
     } else {
-        // Agar koi class nahi hai, pehle click pe 'dark' add karo
         document.body.classList.add('dark');
     }
 });
