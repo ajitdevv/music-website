d736bd608a20838b2b7a5213046d73ecd3971081
ajitdevv <ajitdev014@gmail.com>
2025-09-17 11:10:21 +0530

    commit

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 //! add extra ====> add your card using only name and pitcher
 let btn = document.querySelector(".btnn");
 btn.addEventListener("click", function () {
-    // if (document.body.classList.contains('dark')) {
-    //     document.body.classList.remove('dark');
-    //     document.body.classList.add('light');
-    // } else if (document.body.classList.contains('light')) {     
-    //     document.body.classList.remove('light');
-    //     document.body.classList.add('dark');
+    if (document.body.classList.contains('dark')) {
+        document.body.classList.remove('dark');
+        document.body.classList.add('light');
+    } else if (document.body.classList.contains('light')) {     
+        document.body.classList.remove('light');
+        document.body.classList.add('dark');
+        alert("Dark mode is activated");
+        
     // } else {
     //     document.body.classList.add('dark');
-    // }
+    }
 });
 
 
