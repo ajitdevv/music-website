a3f21b526169ada73b2a7c438c6584ad7f5e7a31
ajitdevv <ajitdev014@gmail.com>
2025-09-20 14:36:44 +0530

    commit

--- script.js
+++ script.js
@@ -5,83 +5,83 @@
 //***** Theme chang *****//
 
 
-let btn = document.querySelector(".theme-chang");
-btn.addEventListener("click", function () {
-    if (document.body.classList.contains('dark')) {
-        document.body.classList.remove('dark');
-        document.body.classList.add('light');
-    } else {
-        document.body.classList.remove('light');
-        document.body.classList.add('dark');
-        alert("Look like paid website");
-    }
-});
+// let btn = document.querySelector(".theme-chang");
+// btn.addEventListener("click", function () {
+//     if (document.body.classList.contains('dark')) {
+//         document.body.classList.remove('dark');
+//         document.body.classList.add('light');
+//     } else {
+//         document.body.classList.remove('light');
+//         document.body.classList.add('dark');
+//         alert("Look like paid website");
+//     }
+// });
 
 
 
-//***** Song search *****//
+// //***** Song search *****//
 
-const container = document.getElementsByClassName("music")[0];
-const searchInput = document.getElementsByClassName("search_input")[0];
+// const container = document.getElementsByClassName("music")[0];
+// const searchInput = document.getElementsByClassName("search_input")[0];
 
-let allSongs = [];
-
-
-fetch("songs.json")
-    .then(res => res.json())
-    .then(data => {
-        allSongs = data;
-        const randomThree = getRandomSongs(allSongs, 3);
-        renderSongs(randomThree);
-    });
+// let allSongs = [];
 
 
-function getRandomSongs(arr, n) {
-    const shuffled = arr.slice().sort(() => 0.5 - Math.random());
-    return shuffled.slice(0, n);
-}
+// fetch("songs.json")
+//     .then(res => res.json())
+//     .then(data => {
+//         allSongs = data;
+//         const randomThree = getRandomSongs(allSongs, 3);
+//         renderSongs(randomThree);
+//     });
 
 
+// function getRandomSongs(arr, n) {
+//     const shuffled = arr.slice().sort(() => 0.5 - Math.random());
+//     return shuffled.slice(0, n);
+// }
 
 
 
 
-function renderSongs(songList) {
-    container.innerHTML = "";
-    songList.forEach(song => {
-        const card = document.createElement("div");
-        card.className = "song-card";
-        card.style.backgroundImage = `url(${song.image})`;
-
-        const overlay = document.createElement("div");
-        overlay.className = "overlay";
-
-        const title = document.createElement("h3");
-        title.textContent = `${song.name} -— ${song.singer}`;
-
-        const audio = document.createElement("audio");
-        audio.controls = true;
-        audio.src = song.src;
-
-        overlay.appendChild(title);
-        overlay.appendChild(audio);
-        card.appendChild(overlay);
-        container.appendChild(card);
-    });
-}
 
 
-searchInput.addEventListener("input", () => {
-    const query = searchInput.value.toLowerCase();
-    const filtered = allSongs.filter(song =>
-        song.name.toLowerCase().includes(query) ||
-        song.singer.toLowerCase().includes(query) ||
-        song.other.toLowerCase().includes(query)
-    );
+// function renderSongs(songList) {
+//     container.innerHTML = "";
+//     songList.forEach(song => {
+//         const card = document.createElement("div");
+//         card.className = "song-card";
+//         card.style.backgroundImage = `url(${song.image})`;
 
-    renderSongs(filtered);
+//         const overlay = document.createElement("div");
+//         overlay.className = "overlay";
 
-});
+//         const title = document.createElement("h3");
+//         title.textContent = `${song.name} -— ${song.singer}`;
+
+//         const audio = document.createElement("audio");
+//         audio.controls = true;
+//         audio.src = song.src;
+
+//         overlay.appendChild(title);
+//         overlay.appendChild(audio);
+//         card.appendChild(overlay);
+//         container.appendChild(card);
+//     });
+// }
+
+
+// searchInput.addEventListener("input", () => {
+//     const query = searchInput.value.toLowerCase();
+//     const filtered = allSongs.filter(song =>
+//         song.name.toLowerCase().includes(query) ||
+//         song.singer.toLowerCase().includes(query) ||
+//         song.other.toLowerCase().includes(query)
+//     );
+
+//     renderSongs(filtered);
+
+// });
 
 
 
